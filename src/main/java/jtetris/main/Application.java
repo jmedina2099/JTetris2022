9f0b3e7b9d6931c5fdb2db6350e557bb9bff1dad
jmedina <jmedina@debian.localhost>
2022-07-21 16:05:08 -0500

    Fix

--- src/main/java/jtetris/main/Application.java
+++ src/main/java/jtetris/main/Application.java
@@ -35,7 +35,7 @@ public class Application {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://" + getExternalIP() + ":3000");
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://" + getExternalIP() + ":3000","http://13.52.252.10:3000");
             }
         };
     }
