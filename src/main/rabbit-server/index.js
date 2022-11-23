798ac6ce11642d8ad6a386b48d32f3f39e4b6362
jmedina <jmedina@debian.localhost>
2022-11-23 14:31:34 -0600

    Using tls for rabbitmq adding certs and keys

--- src/main/rabbit-server/index.js
+++ src/main/rabbit-server/index.js
@@ -6,6 +6,8 @@ import express from "express";
 import http from "http";
 import amqp from 'amqplib/callback_api.js';
 import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
 
 var port = 4000;
 
@@ -22,9 +24,20 @@ server.listen(port);
 var socketConection = [];
 connection(io,socketConection);
 
+//var rabbitHost = 'localhost:5672';
+//var rabbitHost = 'jtetrisapprabbitmqserver.azurewebsites.net:80';
 var rabbitHost = 'jtetrisapprabbitmqserver.azurewebsites.net:443';
 
-amqp.connect('amqp://'+rabbitHost, function(error0, connection) {
+var opts = {
+    cert: fs.readFileSync(path.resolve( './', './client.crt')),
+    key: fs.readFileSync(path.resolve( './', './client.key')),
+    ca: [fs.readFileSync(path.resolve( './', './serverca.crt')),
+         fs.readFileSync(path.resolve( './', './azureca1.pem')),
+         fs.readFileSync(path.resolve( './', './azureca2.pem'))]
+};
+
+amqp.connect('amqps://guest:guest@'+rabbitHost, opts, function(error0, connection) {
+//amqp.connect('amqp://guest:guest@'+rabbitHost, function(error0, connection) {
     if (error0) {
         throw error0;
     }
