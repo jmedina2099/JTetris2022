972762c934d5044c41ffad7ea540419c9304567f
jmedina <jmedina@debian.localhost>
2022-11-24 11:51:20 -0600

    Coupled rabbimq client & server

--- src/main/rabbit-server/index.js
+++ src/main/rabbit-server/index.js
@@ -5,6 +5,9 @@ import { Server } from "socket.io";
 import express from "express";
 import http from "http";
 import cors from 'cors';
+import amqp from 'amqplib/callback_api.js';
+import fs from 'fs';
+import path from 'path';
 
 var port = 4000;
 
@@ -32,4 +35,121 @@ server.listen(port);
 var socketConection = [];
 connection(io,socketConection);
 
-console.log( 'DONE setting listener' );
+var rabbitHost = 'localhost:5672';
+//var rabbitHost = 'jtetrisapprabbitmqserver.azurewebsites.net:80';
+//var rabbitHost = 'jtetrisapprabbitmqserver.azurewebsites.net:443';
+
+var urlObj = {
+    protocol: 'amqps',
+    hostname: 'jtetrisapprabbitmqserver.azurewebsites.net',
+    port: 80,
+    username: 'jmedina',
+    password: 'jmedina',
+    locale: 'en_US',
+    frameMax: 0,
+    heartbeat: 0,
+    vhost: '/',
+}
+
+var opts = {
+    cert: fs.readFileSync(path.resolve( './', './client.crt')),
+    key:  fs.readFileSync(path.resolve( './', './client.key')),
+    ca: [fs.readFileSync(path.resolve( './', './autoridad1.crt')),
+         fs.readFileSync(path.resolve( './', './autoridad2.crt')),
+         fs.readFileSync(path.resolve( './', './azureca1.pem')),
+         fs.readFileSync(path.resolve( './', './azureca2.pem'))]
+};
+
+console.log( 'Connecting to rabbitmq...' );
+amqp.connect(urlObj, opts, function(error0, connection) {
+    console.log( 'Answering..' );
+    if (error0) {
+        throw error0;
+    }
+    console.log( 'No errors..' );
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var queue = 'score-queue';
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, function(msg) {
+            if( socketConection[0] ) {
+                socketConection[0].sendScore(msg.content.toString());
+            }
+        }, {
+            noAck: true
+        });
+    });
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var queue = 'board-queue';
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, function(msg) {
+            if( socketConection[0] ) {
+                socketConection[0].sendBoard(msg.content.toString());
+            }
+        }, {
+            noAck: true
+        });
+    });
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var queue = 'hash-board-queue';
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, function(msg) {
+            if( socketConection[0] ) {
+                socketConection[0].sendHash(msg.content.toString());
+            }
+        }, {
+            noAck: true
+        });
+    });
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var queue = 'figures-queue';
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, function(msg) {
+            if( socketConection[0] ) {
+                socketConection[0].sendFigures(msg.content.toString());
+            }
+        }, {
+            noAck: true
+        });
+    });
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var queue = 'figure-falling-queue';
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, function(msg) {
+            if( socketConection[0] ) {
+                socketConection[0].sendFigureFalling(msg.content.toString());
+            }
+        }, {
+            noAck: true
+        });
+    });
+});
