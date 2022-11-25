b0b1ab5fd707f9800e84fe4b37b50aad754f9294
jmedina <jmedina@debian.localhost>
2022-11-24 21:54:43 -0600

    Testing..

--- src/main/rabbit-server/index.js
+++ src/main/rabbit-server/index.js
@@ -60,6 +60,7 @@ var opts = {
          fs.readFileSync(path.resolve( './', './azureca2.pem'))]
 };
 
+/*
 console.log( 'Connecting to rabbitmq...' );
 amqp.connect(urlObj, opts, function(error0, connection) {
     console.log( 'Answering..' );
@@ -153,3 +154,4 @@ amqp.connect(urlObj, opts, function(error0, connection) {
         });
     });
 });
+*/
